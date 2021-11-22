8cea39adcc42d32ab3a831eb7959ffc44e5feddf
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-11-22 15:03:59 +0200

    add kata 8 and 7 kyu

---
+++ JavaScript/7kyu/Vowel Count.js
@@ -0,0 +1,17 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  var vowelsCount = 0;
+  // enter your majic here
+  let vowels = ["a", "e", "i", "o", "u"];
+  for (let letter of str) {
+    if(vowels.includes(letter)) {
+      vowelsCount++;
+    }
+  }
+  return vowelsCount;
+}
---
+++ JavaScript/8kyu/Opposite number.js
@@ -0,0 +1,12 @@
+// Very simple, given a number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  //your code here
+  return -number;
+}
