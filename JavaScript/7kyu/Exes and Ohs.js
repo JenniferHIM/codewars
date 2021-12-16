b49722b5774d046b8d18e31052f2ad059ebfdcc8
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-12-16 16:21:58 +0200

    add 7kyu

---
+++ JavaScript/7kyu/Exes and Ohs.js
@@ -0,0 +1,37 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. 
+// The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+  let x = str.match(/x/gi);
+  let o = str.match(/o/gi);
+  return (x && x.length) === (o && o.length);
+}
+
+or
+
+const XO = str => {
+  str = str.toLowerCase().split('');
+  return str.filter(x => x === 'x').length === str.filter(x => x === 'o').length;
+}
+
+or
+
+function XO(str) {
+    var a = str.replace(/x/gi, ''),
+        b = str.replace(/o/gi, '');
+    return a.length === b.length;
+}
+
+or
+
+function XO(str) {
+    return str.toLowerCase().split('x').length === str.toLowerCase().split('o').length;
+}
